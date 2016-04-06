9b3310783d95bd421b9d02252c28b20e36eca49b
Benjamin Doherty <bjd@bangpound.org>
2016-04-06 18:56:44 -0500

    initial commit.

---
+++ src/functions.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Drupal\adjustable_titles;
+
+use Symfony\Cmf\Component\Routing\RouteObjectInterface;
+
+function get_node()
+{
+    $request = \Drupal::request();
+
+    if ($request->attributes->has('node')) {
+        if (\Drupal::request()->attributes->get(RouteObjectInterface::ROUTE_NAME) == 'node.view') {
+            return $request->attributes->get('node');
+        } else {
+            if (\Drupal::request()->attributes->get(RouteObjectInterface::ROUTE_NAME) == 'entity.node.revision') {
+                $vid = \Drupal::request()->attributes->get('node_revision');
+
+                return \Drupal::entityManager()
+                  ->getStorage('node')
+                  ->loadRevision($vid);
+            }
+        }
+    }
+}
